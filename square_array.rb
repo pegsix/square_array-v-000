1d8948f080dbdc135040e5ae977c5deb2a549401
Peggy Lee <peggyleesix@gmail.com>
2018-06-23 22:32:31 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,13 @@
+# def square_array(array)
+#     array.each do |num|
+#       num**2
+#
+#     new_numbers = Array.new
+#     new_numbers << num**2
+#     end
+# end
 def square_array(array)
     array.each do |num|
-      num**2
-      
-    new_numbers = Array.new
-    new_numbers << num**2
+    num**2
     end
 end
